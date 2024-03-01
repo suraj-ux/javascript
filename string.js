91a59a8d3d09bea0b6a77c3adbcd03e9018f7ce1
suraj-ux <62553256+suraj-ux@users.noreply.github.com>
2024-03-01 17:49:42 +0000

    learning string

--- string.js
+++ string.js
@@ -6,6 +6,11 @@ const age = 29;
 
 console.log(`His Name is ${userName} his age is ${age}`); // This is a good way of string
  
-const employeeName = new String(`Mohan`);
+const employeeName = new String("Mohan");
 
-console.log();
+console.log(employeeName.toUpperCase());
+
+// Here is the new intresting level of string that we use string and its alphabet position level
+
+console.log(employeeName.__proto__);
+console.log(employeeName.blink());
