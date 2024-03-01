10e68573f9203ad8355814550527c2e39bffa0bb
suraj-ux <62553256+suraj-ux@users.noreply.github.com>
2024-03-01 19:23:08 +0000

    we learn date js

---
+++ dates.js
@@ -0,0 +1,7 @@
+// Here we learn about date
+
+const myDate = new Date();
+console.table([myDate]);
+console.log(myDate.getSeconds());
+//  const createDate = new Date(2024,2, 2);
+//  console.log(createDate.toDateString());
