1aacadfd131c65ce419213c30fa47a85b3e03c70
suraj-ux <62553256+suraj-ux@users.noreply.github.com>
2024-03-01 16:56:07 +0000

    Learning string

---
+++ string.js
@@ -0,0 +1,11 @@
+// here We learn About string
+
+const userName = 'suraj';
+const age = 29;
+// console.log( 'His name is ' +userName+ ' his age is' + age); // this is a bad way to use string
+
+console.log(`His Name is ${userName} his age is ${age}`); // This is a good way of string
+ 
+const employeeName = new String(`Mohan`);
+
+console.log();
