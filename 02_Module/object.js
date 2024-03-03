61ffe7b42a0a552053bb81d6ddb177ba98528708
suraj-ux <62553256+suraj-ux@users.noreply.github.com>
2024-03-03 08:44:04 +0000

    here we learn objects

---
+++ 02_Module/object.js
@@ -0,0 +1,8 @@
+// here we learn object 
+
+const fbuser ={}  //this is the singletone object make with (constructor)
+fbuser.Id = 26,
+fbuser.Name = 'sk',
+fbuser.Lastname = 'jha',
+
+console.table ([fbuser]);
